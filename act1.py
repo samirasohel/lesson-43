98b6555e7abe733acedcfe4e8a917e4fc7c9001f
samirasohel <samirasohel.othoi@gmail.com>
2024-12-13 11:36:04 +0600

    act 1

---
+++ act1.py
@@ -0,0 +1,17 @@
+list1=["mango","apple","cherry","kiwi"]
+print("list 1 is",list1)
+
+list1.append("watermelon")
+print("updated list 1 is",list1)
+
+list1.remove("mango")
+print("updated list 1 is after removed",list1)
+
+print("length of the list is",len(list1))
+
+print("I like",list1[3])
+
+list2=[1,2,3,4,5]
+print("sliced list is",list2[1:2])
+
+print(list1.clear())
